c24b719ea8bdab784d153391a2f4bccb9e628942
Zayar Myo Thwin <zayarmyothwin@gmail.com>
2023-10-26 11:51:36 +0630

    added array ex

---
+++ array_ex5.py
@@ -0,0 +1,3 @@
+list=[1,5,2,7,8,9,200,155]
+for(i,item) in enumerate(list):
+    print("Index:",i,"And Vale:",item)
---
+++ array_maxnumber.py
@@ -0,0 +1,6 @@
+list=[1048,1255,2125,1050,2506,1236,2010,1055]
+maxnumber=list[0]
+for x in list:
+    if maxnumber<x:
+        maxnumber=x
+print("Max number in array is",maxnumber)
